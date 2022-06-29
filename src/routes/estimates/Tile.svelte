d9a91cf04d31bf001e04f86117e04b5b65ed8c2a
Philip Lewis <philipjonlewis@gmail.com>
2022-06-29 23:53:19 +0800

    added tile computations

--- src/routes/estimates/Tile.svelte
+++ src/routes/estimates/Tile.svelte
@@ -1,15 +1,81 @@
-<script>
+<script lang="ts">
+  let unitOfMeasurement;
+  // let surfaceArea = 0;
+  let floorArea = 0;
+  let contingency;
+  let tileDimensions = {
+    width: 0,
+    height: 0,
+  };
+
+  $: convertedContingency =
+    Math.ceil((floorArea / surfaceArea) * toDecimal(contingency)) || 0;
+
+  $: surfaceArea = tileDimensions.width * tileDimensions.height;
+
+  $: totalNumberOfTiles =
+    Math.ceil(floorArea / surfaceArea) + convertedContingency || 0;
+
+  // $: console.log(unitOfMeasurement);
+  // $: console.log(surfaceArea);
+  $: console.log(totalNumberOfTiles);
+
+  function toDecimal(num: any) {
+    return parseFloat(num) / 100;
+  }
 </script>
 
 <!-- <ContentLayout >Tiles</ContentLayout> -->
 
 <div>
-  <h5>User Input</h5>
-  <p>Unit of Measurement</p>
-  <p>Surface Area</p>
-  <p>Tile Area</p>
+  <div>
+    <p>Tiles : {Math.ceil(floorArea / surfaceArea) || 0}</p>
+    <p>Contingency : {convertedContingency}</p>
+
+    <p>
+      Total Number of tiles = {totalNumberOfTiles}
+    </p>
+  </div>
+  <br />
   <hr />
-  <h5>Output</h5>
+  <div class="unit-of-measurement">
+    <p>
+      Unit of Measurement : {unitOfMeasurement} - {unitOfMeasurement ==
+      "Imperial"
+        ? "ft / In"
+        : "mm / cm"}
+    </p>
+    <select bind:value={unitOfMeasurement}>
+      <option value={"Metric"}>Metric</option>
+      <option value={"Imperial"}>Imperial</option>
+    </select>
+  </div>
+  <br />
+  <hr />
+  <div class="floor-area">
+    <form>
+      <label for="floorArea">Floor Area</label>
+      <input type="number" id="floorArea" bind:value={floorArea} />
+    </form>
+  </div>
+  <br />
+  <hr />
+  <div class="tile-details">
+    <form class="flex flex-col w-64">
+      <p>Tile Surface Area = {surfaceArea}</p>
+      <label for="tileWidth">Tile Width</label>
+      <input type="number" id="tileWidth" bind:value={tileDimensions.width} />
+      <label for="tileHeight">Tile Height</label>
+      <input type="number" id="tileHeight" bind:value={tileDimensions.height} />
+      <label for="contingency">contingency in percentage</label>
+      <input type="number" id="contingency" bind:value={contingency} />
+    </form>
+  </div>
+
+  <!-- <h5>Output</h5>
   <p>Tile Adhesive</p>
-  <p>Tile Grout</p>
+  <p>Tile Grout</p> -->
 </div>
+
+<style lang="scss">
+</style>
---
+++ src/store/TileStore.svelte
@@ -0,0 +1,33 @@
+<script>
+  // your script goes here
+  import { writable } from "svelte/store";
+
+  const tileValues = writable({
+    unitOfMeasurement: "Metric",
+    tileDimensions: {
+      width: 0,
+      height: 0,
+      area: 0,
+    },
+    surfaceArea: 0,
+    contingency: 0,
+  });
+
+  //   export const getTileArea = (obj) => {
+  //     const tileArea = obj.tileDimensions.width * obj.tileDimensions.height;
+  //     return {
+  //       ...obj,
+  //       tileDimensions: { ...obj.tileDimensions, area: tileArea },
+  //     };
+  //   };
+
+  export { tileValues };
+
+  //   count.subscribe((value) => {
+  //     console.log(value);
+  //   }); // logs '0'
+
+  //   count.set(1); // logs '1'
+
+  //   count.update((n) => n + 1); // logs '2'
+</script>
